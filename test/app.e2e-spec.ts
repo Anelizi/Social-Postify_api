fdfcab1118d0da33bb0d719a89d6d8295eac9ec8
Anelizi <anelizicarvalho@gmail.com>
2023-08-28 17:17:45 -0300

    initial: test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, PrismaModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    prisma = await moduleFixture.resolve(PrismaService);
+    await prisma.media.deleteMany();
+    await prisma.post.deleteMany();
+    await prisma.publication.deleteMany();
+
     await app.init();
   });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/health')
       .expect(200)
-      .expect('Hello World!');
+      .expect("I'm okay!");
   });
+
+  it('GET /medias', async () => {
+    //setup
+    await prisma.media.create({
+      data: {
+        title: "Test Client",
+        username: "Test"
+      }
+    });
+
+    let response = await request(app.getHttpServer()).get('/medias');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveLength(1);
+  });
+
 });
